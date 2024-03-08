44b765e817477ce7e32a17438ac77a67f74bc81a
Tokard <adrien.cgeix@gmail.com>
2024-03-08 15:04:03 +0100

    Premier commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Portfolio React
+
+This is a remake of my personal website using React with Next.js and Tailwind CSS.
+
+## Features
+
+- **Responsive Design**: The website is designed to be fully responsive, ensuring a great user experience across all devices.
+- **Next.js**: Built with Next.js, providing server-side rendering and other powerful features for React applications.
+- **Tailwind CSS**: Styled with Tailwind CSS, allowing for easy customization and rapid development.
---
+++ src/app/page.tsx
@@ -0,0 +1,56 @@
+'use client';
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+
+const inter = Inter({ subsets: ["latin"] });
+
+import { useEffect, useRef } from "react";
+
+export default function Home() {
+  const mainBackGradient = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const updateMousePosition = (ev: MouseEvent) => {
+      if (!mainBackGradient.current) return;
+      const { clientX, clientY } = ev;
+      mainBackGradient.current.style.setProperty("--x", `${clientX}px`);
+      mainBackGradient.current.style.setProperty("--y", `${clientY}px`);
+    };
+
+    window.addEventListener("mousemove", updateMousePosition);
+
+    return () => {
+      window.removeEventListener("mousemove", updateMousePosition);
+    };
+  }, []);
+
+  return (
+    <>
+      <style jsx>{`
+        .followGradient {
+          height: 100vh;
+          width: 100%;
+          background-image: radial-gradient(
+            circle farthest-side at var(--x, 100px) var(--y, 100px),
+            #23585d 0%,
+            transparent 40%
+          );
+        }
+      `}</style>
+
+      <main ref={mainBackGradient} className="followGradient flex min-h-screen flex-col items-left justify-between p-24">
+      <div className="z-10 max-w-5xl w-full justify-between font-mono text-sm lg:flex">
+          <h1 className="text-4xl md:text-5xl font-bold text-center lg:text-left text-[#EDEDE8] lg:text-6xl">
+            Adrien Augeix
+            <p className="text-2xl md:text-4xl font-bold bg-clip-text lg:text-4xl">
+              Developer
+            </p>
+        </h1>
+      </div>
+    </main>
+    </>
+  );
+}
+
