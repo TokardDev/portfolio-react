725167e2777ad4a91a6740eb48fa97980cee489a
Tokard <adrien@augeix.fr>
2024-03-13 15:45:25 +0100

    updated mobile version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,19 +38,25 @@ export default function Home() {
             transparent 40%
           );
         }
+
+        @media (max-width: 768px) {
+          .followGradient {
+            background-image: linear-gradient(#000000, #23585d);
+            background-size: cover;
+          }
+        }
       `}</style>
 
       <main ref={mainBackGradient} className="followGradient flex min-h-screen flex-col items-left justify-between p-24">
-      <div className="z-10 max-w-5xl w-full justify-between font-mono text-sm lg:flex">
+        <div className="z-10 max-w-5xl w-full justify-between font-mono text-sm lg:flex">
           <h1 className="text-4xl md:text-5xl font-bold text-center lg:text-left text-[#EDEDE8] lg:text-6xl">
             Adrien Augeix
             <p className="text-2xl md:text-4xl font-bold bg-clip-text lg:text-4xl">
               Developer
             </p>
-        </h1>
-      </div>
-    </main>
+          </h1>
+        </div>
+      </main>
     </>
   );
 }
-
