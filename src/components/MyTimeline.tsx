3ec2c1961d78a4e669d4341beb8fff1b1869acd7
Tokard <adrien@augeix.fr>
2024-03-18 15:36:52 +0100

    Add atropos

--- src/components/MyTimeline.tsx
+++ src/components/MyTimeline.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import { Atropos } from 'atropos/react';
 import './MyTimelineStyle.css';
+import 'atropos/atropos.min.css';
 
 function MyTimeline() {
   return (
     <div style={{ maxHeight: '100vh', overflowY: 'auto' }}>
+
       <VerticalTimeline>
   <VerticalTimelineElement>
   </VerticalTimelineElement>
@@ -12,108 +15,150 @@ function MyTimeline() {
   </VerticalTimelineElement>
   <VerticalTimelineElement
     className="vertical-timeline-element--work"
-    contentStyle={{ background: 'rgba(80, 80, 80, 0.6)', color: '#fff' }}
-    contentArrowStyle={{ borderRight: '7px solid  rgba(80, 80, 80, 0.6)' }}
+    contentStyle={{ background: 'rgba(80, 80, 80, 0)', color: '#fff' }}
+    contentArrowStyle={{ borderRight: '7px solid  rgba(80, 80, 80, 0)' }}
     date="project date"
     iconStyle={{ background: '#303030', color: '#000' }}
     visible={true}
     /* icon={<WorkIcon />} */
   >
+    <Atropos
+        shadow={false}
+      >
+    <div style={{background: 'rgba(80, 80, 80, 0.6)', color: '#fff', padding: '20px', borderRadius: '10px'}}>
     <h3 className="vertical-timeline-element-title" style={{fontSize: "25px"}}>Project Name</h3>
     <h4 className="vertical-timeline-element-subtitle" style={{fontSize: "20px", color: '#8c8c8c'}}>Subtitle</h4>
     <p>
       project desc
     </p>
+    </div>
+    </Atropos>
   </VerticalTimelineElement>
   <VerticalTimelineElement
     className="vertical-timeline-element--work"
-    contentStyle={{ background: 'rgba(80, 80, 80, 0.6)', color: '#fff' }}
-    contentArrowStyle={{ borderRight: '7px solid  rgba(80, 80, 80, 0.6)' }}
+    contentStyle={{ background: 'rgba(80, 80, 80, 0)', color: '#fff' }}
+    contentArrowStyle={{ borderRight: '7px solid  rgba(80, 80, 80, 0)' }}
     date="project date"
     iconStyle={{ background: '#303030', color: '#000' }}
     visible={true}
     /* icon={<WorkIcon />} */
   >
+    <Atropos
+        shadow={false}
+      >
+    <div style={{background: 'rgba(80, 80, 80, 0.6)', color: '#fff', padding: '20px', borderRadius: '10px'}}>
     <h3 className="vertical-timeline-element-title" style={{fontSize: "25px"}}>Project Name</h3>
     <h4 className="vertical-timeline-element-subtitle" style={{fontSize: "20px", color: '#8c8c8c'}}>Subtitle</h4>
     <p>
       project desc
     </p>
+    </div>
+    </Atropos>
   </VerticalTimelineElement>
   <VerticalTimelineElement
     className="vertical-timeline-element--work"
-    contentStyle={{ background: 'rgba(80, 80, 80, 0.6)', color: '#fff' }}
-    contentArrowStyle={{ borderRight: '7px solid  rgba(80, 80, 80, 0.6)' }}
+    contentStyle={{ background: 'rgba(80, 80, 80, 0)', color: '#fff' }}
+    contentArrowStyle={{ borderRight: '7px solid  rgba(80, 80, 80, 0)' }}
     date="project date"
     iconStyle={{ background: '#303030', color: '#000' }}
     visible={true}
     /* icon={<WorkIcon />} */
   >
+    <Atropos
+        shadow={false}
+      >
+    <div style={{background: 'rgba(80, 80, 80, 0.6)', color: '#fff', padding: '20px', borderRadius: '10px'}}>
     <h3 className="vertical-timeline-element-title" style={{fontSize: "25px"}}>Project Name</h3>
     <h4 className="vertical-timeline-element-subtitle" style={{fontSize: "20px", color: '#8c8c8c'}}>Subtitle</h4>
     <p>
       project desc
     </p>
+    </div>
+    </Atropos>
   </VerticalTimelineElement>
   <VerticalTimelineElement
     className="vertical-timeline-element--work"
-    contentStyle={{ background: 'rgba(80, 80, 80, 0.6)', color: '#fff' }}
-    contentArrowStyle={{ borderRight: '7px solid  rgba(80, 80, 80, 0.6)' }}
+    contentStyle={{ background: 'rgba(80, 80, 80, 0)', color: '#fff' }}
+    contentArrowStyle={{ borderRight: '7px solid  rgba(80, 80, 80, 0)' }}
     date="project date"
     iconStyle={{ background: '#303030', color: '#000' }}
     visible={true}
     /* icon={<WorkIcon />} */
   >
+    <Atropos
+        shadow={false}
+      >
+    <div style={{background: 'rgba(80, 80, 80, 0.6)', color: '#fff', padding: '20px', borderRadius: '10px'}}>
     <h3 className="vertical-timeline-element-title" style={{fontSize: "25px"}}>Project Name</h3>
     <h4 className="vertical-timeline-element-subtitle" style={{fontSize: "20px", color: '#8c8c8c'}}>Subtitle</h4>
     <p>
       project desc
     </p>
+    </div>
+    </Atropos>
   </VerticalTimelineElement>
   <VerticalTimelineElement
     className="vertical-timeline-element--work"
-    contentStyle={{ background: 'rgba(80, 80, 80, 0.6)', color: '#fff' }}
-    contentArrowStyle={{ borderRight: '7px solid  rgba(80, 80, 80, 0.6)' }}
+    contentStyle={{ background: 'rgba(80, 80, 80, 0)', color: '#fff' }}
+    contentArrowStyle={{ borderRight: '7px solid  rgba(80, 80, 80, 0)' }}
     date="project date"
     iconStyle={{ background: '#303030', color: '#000' }}
     visible={true}
     /* icon={<WorkIcon />} */
   >
+    <Atropos
+        shadow={false}
+      >
+    <div style={{background: 'rgba(80, 80, 80, 0.6)', color: '#fff', padding: '20px', borderRadius: '10px'}}>
     <h3 className="vertical-timeline-element-title" style={{fontSize: "25px"}}>Project Name</h3>
     <h4 className="vertical-timeline-element-subtitle" style={{fontSize: "20px", color: '#8c8c8c'}}>Subtitle</h4>
     <p>
       project desc
     </p>
+    </div>
+    </Atropos>
   </VerticalTimelineElement>
   <VerticalTimelineElement
     className="vertical-timeline-element--work"
-    contentStyle={{ background: 'rgba(80, 80, 80, 0.6)', color: '#fff' }}
-    contentArrowStyle={{ borderRight: '7px solid  rgba(80, 80, 80, 0.6)' }}
+    contentStyle={{ background: 'rgba(80, 80, 80, 0)', color: '#fff' }}
+    contentArrowStyle={{ borderRight: '7px solid  rgba(80, 80, 80, 0)' }}
     date="project date"
     iconStyle={{ background: '#303030', color: '#000' }}
     visible={true}
     /* icon={<WorkIcon />} */
   >
+    <Atropos
+        shadow={false}
+      >
+    <div style={{background: 'rgba(80, 80, 80, 0.6)', color: '#fff', padding: '20px', borderRadius: '10px'}}>
     <h3 className="vertical-timeline-element-title" style={{fontSize: "25px"}}>Project Name</h3>
     <h4 className="vertical-timeline-element-subtitle" style={{fontSize: "20px", color: '#8c8c8c'}}>Subtitle</h4>
     <p>
       project desc
     </p>
+    </div>
+    </Atropos>
   </VerticalTimelineElement>
   <VerticalTimelineElement
     className="vertical-timeline-element--work"
-    contentStyle={{ background: 'rgba(80, 80, 80, 0.6)', color: '#fff' }}
-    contentArrowStyle={{ borderRight: '7px solid  rgba(80, 80, 80, 0.6)' }}
+    contentStyle={{ background: 'rgba(80, 80, 80, 0)', color: '#fff' }}
+    contentArrowStyle={{ borderRight: '7px solid  rgba(80, 80, 80, 0)' }}
     date="project date"
     iconStyle={{ background: '#303030', color: '#000' }}
     visible={true}
     /* icon={<WorkIcon />} */
   >
+    <Atropos
+        shadow={false}
+      >
+    <div style={{background: 'rgba(80, 80, 80, 0.6)', color: '#fff', padding: '20px', borderRadius: '10px'}}>
     <h3 className="vertical-timeline-element-title" style={{fontSize: "25px"}}>Project Name</h3>
     <h4 className="vertical-timeline-element-subtitle" style={{fontSize: "20px", color: '#8c8c8c'}}>Subtitle</h4>
     <p>
       project desc
     </p>
+    </div>
+    </Atropos>
   </VerticalTimelineElement>
 </VerticalTimeline>
     </div>
