b2fac9edb8c72dc93e3484ad52e3a5979d2b3919
Tokard <adrien.cgeix@gmail.com>
2024-03-20 00:40:50 +0100

    Add styles for atropos-inner element and update package.json

--- src/components/MyTimeline.tsx
+++ src/components/MyTimeline.tsx
@@ -1,166 +1,95 @@
-import React from 'react';
-import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import React, { useState, useEffect } from 'react';
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import { Atropos } from 'atropos/react';
 import './MyTimelineStyle.css';
 import 'atropos/atropos.min.css';
 
+import { IconContext } from "react-icons";
+import { TbBrandCSharp } from "react-icons/tb";
+import { SiCplusplus } from "react-icons/si";
+import { FaPython, FaJava, FaReact, FaSquareJs } from "react-icons/fa6";
+import { BiLogoPhp } from "react-icons/bi";
+import { SiGodotengine } from "react-icons/si";
+import { SiArduino } from "react-icons/si";
+import { SiKotlin } from "react-icons/si";
+
+const languageImports ={
+  CSharp: <TbBrandCSharp />,
+  CPlusPlus: <SiCplusplus />,
+  Python: <FaPython />,
+  Java: <FaJava />,
+  React: <FaReact />,
+  JS: <FaSquareJs />,
+  PHP: <BiLogoPhp />,
+  Godot: <SiGodotengine />,
+  Arduino: <SiArduino />, 
+  Kotlin: <SiKotlin />,
+};
+
 function MyTimeline() {
+  const [projects, setProjects] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch('/projects.json')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Failed to fetch projects');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log('Fetched projects:', data); // Log fetched data
+        if (!Array.isArray(data)) {
+          throw new Error('Projects data is not an array');
+        }
+        setProjects(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error('Error fetching projects:', error);
+        setError(error.message);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
   return (
     <div style={{ maxHeight: '100vh', overflowY: 'auto' }}>
-
       <VerticalTimeline>
-  <VerticalTimelineElement>
-  </VerticalTimelineElement>
-  <VerticalTimelineElement>
-  </VerticalTimelineElement>
-  <VerticalTimelineElement
-    className="vertical-timeline-element--work"
-    contentStyle={{ background: 'rgba(80, 80, 80, 0)', color: '#fff' }}
-    contentArrowStyle={{ borderRight: '7px solid  rgba(80, 80, 80, 0)' }}
-    date="project date"
-    iconStyle={{ background: '#303030', color: '#000' }}
-    visible={true}
-    /* icon={<WorkIcon />} */
-  >
-    <Atropos
-        shadow={false}
-      >
-    <div style={{background: 'rgba(80, 80, 80, 0.6)', color: '#fff', padding: '20px', borderRadius: '10px'}}>
-    <h3 className="vertical-timeline-element-title" style={{fontSize: "25px"}}>Project Name</h3>
-    <h4 className="vertical-timeline-element-subtitle" style={{fontSize: "20px", color: '#8c8c8c'}}>Subtitle</h4>
-    <p>
-      project desc
-    </p>
-    </div>
-    </Atropos>
-  </VerticalTimelineElement>
-  <VerticalTimelineElement
-    className="vertical-timeline-element--work"
-    contentStyle={{ background: 'rgba(80, 80, 80, 0)', color: '#fff' }}
-    contentArrowStyle={{ borderRight: '7px solid  rgba(80, 80, 80, 0)' }}
-    date="project date"
-    iconStyle={{ background: '#303030', color: '#000' }}
-    visible={true}
-    /* icon={<WorkIcon />} */
-  >
-    <Atropos
-        shadow={false}
-      >
-    <div style={{background: 'rgba(80, 80, 80, 0.6)', color: '#fff', padding: '20px', borderRadius: '10px'}}>
-    <h3 className="vertical-timeline-element-title" style={{fontSize: "25px"}}>Project Name</h3>
-    <h4 className="vertical-timeline-element-subtitle" style={{fontSize: "20px", color: '#8c8c8c'}}>Subtitle</h4>
-    <p>
-      project desc
-    </p>
-    </div>
-    </Atropos>
-  </VerticalTimelineElement>
-  <VerticalTimelineElement
-    className="vertical-timeline-element--work"
-    contentStyle={{ background: 'rgba(80, 80, 80, 0)', color: '#fff' }}
-    contentArrowStyle={{ borderRight: '7px solid  rgba(80, 80, 80, 0)' }}
-    date="project date"
-    iconStyle={{ background: '#303030', color: '#000' }}
-    visible={true}
-    /* icon={<WorkIcon />} */
-  >
-    <Atropos
-        shadow={false}
-      >
-    <div style={{background: 'rgba(80, 80, 80, 0.6)', color: '#fff', padding: '20px', borderRadius: '10px'}}>
-    <h3 className="vertical-timeline-element-title" style={{fontSize: "25px"}}>Project Name</h3>
-    <h4 className="vertical-timeline-element-subtitle" style={{fontSize: "20px", color: '#8c8c8c'}}>Subtitle</h4>
-    <p>
-      project desc
-    </p>
-    </div>
-    </Atropos>
-  </VerticalTimelineElement>
-  <VerticalTimelineElement
-    className="vertical-timeline-element--work"
-    contentStyle={{ background: 'rgba(80, 80, 80, 0)', color: '#fff' }}
-    contentArrowStyle={{ borderRight: '7px solid  rgba(80, 80, 80, 0)' }}
-    date="project date"
-    iconStyle={{ background: '#303030', color: '#000' }}
-    visible={true}
-    /* icon={<WorkIcon />} */
-  >
-    <Atropos
-        shadow={false}
-      >
-    <div style={{background: 'rgba(80, 80, 80, 0.6)', color: '#fff', padding: '20px', borderRadius: '10px'}}>
-    <h3 className="vertical-timeline-element-title" style={{fontSize: "25px"}}>Project Name</h3>
-    <h4 className="vertical-timeline-element-subtitle" style={{fontSize: "20px", color: '#8c8c8c'}}>Subtitle</h4>
-    <p>
-      project desc
-    </p>
-    </div>
-    </Atropos>
-  </VerticalTimelineElement>
-  <VerticalTimelineElement
-    className="vertical-timeline-element--work"
-    contentStyle={{ background: 'rgba(80, 80, 80, 0)', color: '#fff' }}
-    contentArrowStyle={{ borderRight: '7px solid  rgba(80, 80, 80, 0)' }}
-    date="project date"
-    iconStyle={{ background: '#303030', color: '#000' }}
-    visible={true}
-    /* icon={<WorkIcon />} */
-  >
-    <Atropos
-        shadow={false}
-      >
-    <div style={{background: 'rgba(80, 80, 80, 0.6)', color: '#fff', padding: '20px', borderRadius: '10px'}}>
-    <h3 className="vertical-timeline-element-title" style={{fontSize: "25px"}}>Project Name</h3>
-    <h4 className="vertical-timeline-element-subtitle" style={{fontSize: "20px", color: '#8c8c8c'}}>Subtitle</h4>
-    <p>
-      project desc
-    </p>
-    </div>
-    </Atropos>
-  </VerticalTimelineElement>
-  <VerticalTimelineElement
-    className="vertical-timeline-element--work"
-    contentStyle={{ background: 'rgba(80, 80, 80, 0)', color: '#fff' }}
-    contentArrowStyle={{ borderRight: '7px solid  rgba(80, 80, 80, 0)' }}
-    date="project date"
-    iconStyle={{ background: '#303030', color: '#000' }}
-    visible={true}
-    /* icon={<WorkIcon />} */
-  >
-    <Atropos
-        shadow={false}
-      >
-    <div style={{background: 'rgba(80, 80, 80, 0.6)', color: '#fff', padding: '20px', borderRadius: '10px'}}>
-    <h3 className="vertical-timeline-element-title" style={{fontSize: "25px"}}>Project Name</h3>
-    <h4 className="vertical-timeline-element-subtitle" style={{fontSize: "20px", color: '#8c8c8c'}}>Subtitle</h4>
-    <p>
-      project desc
-    </p>
-    </div>
-    </Atropos>
-  </VerticalTimelineElement>
-  <VerticalTimelineElement
-    className="vertical-timeline-element--work"
-    contentStyle={{ background: 'rgba(80, 80, 80, 0)', color: '#fff' }}
-    contentArrowStyle={{ borderRight: '7px solid  rgba(80, 80, 80, 0)' }}
-    date="project date"
-    iconStyle={{ background: '#303030', color: '#000' }}
-    visible={true}
-    /* icon={<WorkIcon />} */
-  >
-    <Atropos
-        shadow={false}
-      >
-    <div style={{background: 'rgba(80, 80, 80, 0.6)', color: '#fff', padding: '20px', borderRadius: '10px'}}>
-    <h3 className="vertical-timeline-element-title" style={{fontSize: "25px"}}>Project Name</h3>
-    <h4 className="vertical-timeline-element-subtitle" style={{fontSize: "20px", color: '#8c8c8c'}}>Subtitle</h4>
-    <p>
-      project desc
-    </p>
-    </div>
-    </Atropos>
-  </VerticalTimelineElement>
-</VerticalTimeline>
+      <VerticalTimelineElement />
+      <VerticalTimelineElement />
+      <IconContext.Provider value={{ color: "white", className: "global-class-name" }}>
+        {projects.map((project) => (
+          <VerticalTimelineElement
+            key={project.id}
+            className="vertical-timeline-element--work"
+            contentStyle={{ background: 'rgba(80, 80, 80, 0)', color: '#fff' }}
+            contentArrowStyle={{ borderRight: '7px solid  rgba(80, 80, 80, 0)' }}
+            date={project.date}
+            iconStyle={{ background: '#303030', color: '#000' }}
+            visible={true}
+            icon={languageImports[project.lang]}
+          >
+            <Atropos shadow={false}>
+              <div style={{ background: 'rgba(80, 80, 80, 0.6)', color: '#fff', padding: '20px' }}>
+                <h3 className="vertical-timeline-element-title" style={{ fontSize: '25px' }}>{project.title}</h3>
+                <h4 className="vertical-timeline-element-subtitle" style={{ fontSize: '20px', color: '#8c8c8c' }}>{project.subtitle}</h4>
+                <p>{project.description}</p>
+              </div>
+            </Atropos>
+          </VerticalTimelineElement>
+        ))}
+        </IconContext.Provider>
+      </VerticalTimeline>
     </div>
   );
 }
